999816309a6afea019ebb2c35528b2e4eee11cb7
kaspernj <kaspernj@gmail.com>
2018-01-17 08:30:10 +0000

    Worked on getting CI to work

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,14 +8,14 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150103093900) do
+ActiveRecord::Schema.define(version: 20150103093900) do
 
-  create_table "accounts", :force => true do |t|
-    t.string   "iban_string"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "accounts", force: :cascade do |t|
+    t.string "iban_string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
