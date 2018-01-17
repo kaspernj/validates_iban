b26e5432f23f61eb0718b8165c6d8f7742381688
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2018-01-17 08:59:29 +0100

    Add default texts

--- lib/iban_validator.rb
+++ lib/iban_validator.rb
@@ -5,9 +5,7 @@ class IbanValidator < ActiveModel::EachValidator
     iban = IBANTools::IBAN.new(value)
     validation_errors = iban.validation_errors
 
-    unless validation_errors.empty?
-      object.errors[attribute] << (options[:message] || error_string(validation_errors))
-    end
+    object.errors[attribute] << (options[:message] || error_string(validation_errors)) unless validation_errors.empty?
   end
 
   private
@@ -16,12 +14,12 @@ class IbanValidator < ActiveModel::EachValidator
     string = I18n.t('iban.validation.default_message', default: 'is not a valid IBAN string: ')
     validation_errors = validation_errors.map do |error|
       case error.to_sym
-      when :too_short            then I18n.t('iban.validation.too_short')
-      when :bad_chars            then I18n.t('iban.validation.bad_chars')
-      when :bad_check_digits     then I18n.t('iban.validation.bad_check_digits')
-      when :unknown_country_code then I18n.t('iban.validation.unknown_country_code')
-      when :bad_length           then I18n.t('iban.validation.bad_length')
-      when :bad_format           then I18n.t('iban.validation.bad_format')
+      when :too_short            then I18n.t('iban.validation.too_short', default: 'too short')
+      when :bad_chars            then I18n.t('iban.validation.bad_chars', default: 'bad chars')
+      when :bad_check_digits     then I18n.t('iban.validation.bad_check_digits', default: 'bad check digits')
+      when :unknown_country_code then I18n.t('iban.validation.unknown_country_code', default: 'unknown country code')
+      when :bad_length           then I18n.t('iban.validation.bad_length', default: 'bad length')
+      when :bad_format           then I18n.t('iban.validation.bad_format', default: 'bad format')
       end
     end
     string << validation_errors.join(', ').to_s
