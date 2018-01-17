3f4eff717fd3403715a89d94619c1659e94f71c4
kaspernj <kaspernj@gmail.com>
2018-01-17 08:22:31 +0000

    Upgraded gems and fixed specs

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,17 +3,17 @@ require "spec_helper"
 describe Account do
   it "returns too short for short strings" do
     account = Account.new(iban_string: "123")
-    account.valid?.should eq false
-    account.errors.full_messages.should eq ["Iban string is not a valid IBAN string: too_short"]
+    expect(account).to be_invalid
+    expect(account.errors.full_messages).to eq ["Iban string is not a valid IBAN string: too short"]
   end
 
   it "accepts valid strings" do
     account = Account.new(iban_string: "GB82 WEST 1234 5698 7654 32")
-    account.valid?.should eq true
+    expect(account).to be_valid
   end
 
   it "doesnt allow nil-values since they should be done with allow_blank" do
     account = Account.new
-    account.valid?.should eq false
+    expect(account).to be_invalid
   end
 end
--- validates_iban.gemspec
+++ validates_iban.gemspec
@@ -20,5 +20,4 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails"
-  s.add_development_dependency "codeclimate-test-reporter"
 end
